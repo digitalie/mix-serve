01ce03e67a8ee726bfe76cabbfc6bef1f474117f
Shmavon Gazanchyan <sgazanchyan@gmail.com>
2018-06-04 21:52:23 +0100

    Initial version

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const handler = require('serve-handler');
+const http = require('http');
+
+class Serve {
+    /**
+     * All dependencies that should be installed by Mix.
+     *
+     * @return {Array}
+     */
+    dependencies() {
+        return ['serve-handler'];
+    }
+
+    /**
+     * Register the component
+     *
+     * @param {Number} port
+     * @param {String} host
+     * @param {String} pablicDir
+     */
+    register(port = 3000, host = 'localhost', pablicDir = '.') {
+        this.port = port;
+        this.host = host;
+        this.publicDir = publicDir;
+    }
+
+    /**
+     * Boot the component. This method is triggered after the
+     * user's webpack.mix.js file has executed.
+     */
+    boot() {
+        if (this.isWatchMode()) {
+            const server = http.createServer((request, response) =>
+                handler(request, response, { public: this.publicDir })
+            )
+
+            server.listen(this.port, () => {
+              console.log(`Running at http://${this.host}:${this.port}`);
+            });
+        }
+    }
+
+    /**
+     * Check if webpack is running in watch mode
+     *
+     * @returns {Boolean}
+     */
+    isWatchMode() {
+        // at the moment it only checks cli flag,
+        // might need to also check configuration object
+        return process.argv.includes('--watch');
+    }
+}
+
+module.exports = Serve;
